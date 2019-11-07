dbab007d61288f2b3f8cb0effb724efb8e84309d
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2019-11-07 15:27:23 +0000

    Add files via upload

--- src/me/mcblueparrot/serverthings/commands/Unmute.java
+++ src/me/mcblueparrot/serverthings/commands/Unmute.java
@@ -1,37 +1,34 @@
 package me.mcblueparrot.serverthings.commands;
 
-import java.util.ArrayList;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import me.mcblueparrot.serverthings.Main;
 import me.mcblueparrot.serverthings.events.CommandTabCompleter;
 
-public class Mute implements CommandExecutor{
+public class Unmute implements CommandExecutor{
 	
 	private Main plugin;
-	public ArrayList<String> mutes = new ArrayList<String>();
 	
-	public Mute(Main plugin) {
+	public Unmute(Main plugin) {
 		this.plugin = plugin;
-		this.plugin.getCommand("mute").setExecutor(this);
-		this.plugin.getCommand("mute").setTabCompleter(new CommandTabCompleter(this.plugin));
+		this.plugin.getCommand("unmute").setExecutor(this);
+		this.plugin.getCommand("unmute").setTabCompleter(new CommandTabCompleter(this.plugin));
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(sender.hasPermission("serverthings.mute")) {
+		if(sender.hasPermission("serverthings.unmute")) {
 			if(args.length == 0) {
-				sender.sendMessage("§cPlease choose a player to mute");
+				sender.sendMessage("§cPlease choose a player to unmute");
 				return true;
 			}
 			else if(args.length > 0) {
-				if(mutes.contains(args[0])) {
-					sender.sendMessage(args[0] + " is already muted");
+				if(!plugin.mutecommand.mutes.contains(args[0])) {
+					sender.sendMessage(args[0] + " isn't muted");
 					return true;
 				}
-				mutes.add(args[0]);
-				sender.sendMessage("Muted " + args[0]);
+				plugin.mutecommand.mutes.remove(args[0]);
+				sender.sendMessage("Unmuted " + args[0]);
 			}
 			return true;
 		}
